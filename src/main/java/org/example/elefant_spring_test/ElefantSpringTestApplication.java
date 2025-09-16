79202114af308c3ece43000a979a2d5ad542a5f9
salomonxt <salomonxt@protonmail.ch>
2025-09-16 13:20:03 +0200

    Initial commit

---
+++ src/main/java/org/example/elefant_spring_test/ElefantSpringTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.elefant_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElefantSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElefantSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/elefant_spring_test/ElefantSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elefant_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElefantSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
